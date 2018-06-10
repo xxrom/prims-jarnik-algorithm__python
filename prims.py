11578b04a9f350997fed124d99329b2127d7ea28
Chernyshov <xpom55@mail.ru>
2018-06-10 12:43:02 +0300

    added Prims-Jarnik algorithm class/ calculateSpanningTree with all logic

--- prims.py
+++ prims.py
@@ -1,3 +1,17 @@
+# Прима алгоритм O(E * logE) + память для heap O(E)
+
+# используют если полносвязный граф
+
+# Шаги: / (disjointSet.add) == DJS.add
+# 1. выбираем случайную вершину в графе (DJS.add)
+# 2. в heap добавляем все ребра к НЕ посещенным вершинам графа
+# 3. берем минимальное ребро из heap в непосещенную вершину (DJS.add)
+# 4. повторяем шаги 2 и 3, пока heap не пустой
+
+# Для минимального островного дерева (Minimal spanning tree)
+# применяем disjointSet, чтобы быстро проверять, что вершина
+# не принадлежит MST и ее можно добавить в MST
+
 import heapq
 
 class Vertex(object):
@@ -26,4 +40,36 @@ class Edge(object):
     otherPriority = self.weight
 
     return selfPriority < otherPriority
+
+
+class PrimsJarnik(object):
+
+  def __init__(self, unvisitedList):
+    self.unvisitedList = unvisitedList # все не посещенные вершины
+    self.spanningTree = [] # MST
+    self.edgeHeap = [] # heap
+    self.fullCost = 0 # sum of all MST edges
+
+  def calculateSpanningTree(self, vertex): # начальная вершина
+    self.unvisitedList.remove(vertex) # посетили начальную вершину
+
+    while self.unvisitedList: # пока есть не посещенные вершины
+      for edge in vertex.adjacenciesList: # смотрим все ребра vertex
+        if edge.targetVertex in self.unvisitedList: # targ не посещена
+          heapq.heappush(self.edgeHeap, edge) # in edgeHeap add edge
+
+      minEdge = heapq.heappop(self.edgeHeap) # get min edge
+
+      self.spanningTree.append(minEdge) # add in MST
+      print('Edge adding to spanning tree: %s - %s ' % (
+        minEdge.startVertex, minEdge.targetVertex
+      ))
+      self.fullCost += minEdge.weight # sum cost
+
+      vertex = minEdge.targetVertex # set new current Vertex!
+      self.unvisitedList.remove(vertex) # remove visited Vertex!
+
+  def getSpanningTree(self):
+    return self.spanningTree
+
 
